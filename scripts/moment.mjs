076fbbf280ce912c5be1c37079d1ec963bb14fd6
Ayu <hello@ayuhito.com>
2024-01-25 02:08:27 +0000

    feat: update intl data to include old timezones and more common country names

--- scripts/moment.mjs
+++ scripts/moment.mjs
@@ -1,4 +1,4 @@
-import fs from "fs/promises"
+import fs from "fs/promises";
 
 /**
  * @typedef {Record<string, string>} TimezoneMap
@@ -12,27 +12,15 @@ import fs from "fs/promises"
  * Downloads the latest moment timezone data from the Moment Timezone GitHub repository.
  */
 const downloadMoment = async () => {
-	const url = "https://github.com/moment/moment-timezone/raw/develop/data/meta/latest.json"
+  const url =
+    "https://github.com/moment/moment-timezone/raw/develop/data/meta/latest.json";
 
-	const response = await fetch(url)
-	const data = await response.json()
+  const response = await fetch(url);
+  const data = await response.json();
 
-	// Overwrite the existing data/download.json file with the latest data.
-	await fs.writeFile("../data/tz-download.json", JSON.stringify(data, null, 2))
-}
-
-/**
- * Downloads ISO 3166-1 country codes from the ISO 3166 GitHub repository.	 
-*/
-const downloadISO = async () => {
-	const url = "https://raw.githubusercontent.com/biter777/countries/master/data/iso-codes/data_iso_3166-1.json"
-
-	const response = await fetch(url)
-	const data = await response.json()
-
-	// Overwrite the existing data/iso.json file with the latest data.
-	await fs.writeFile("../data/iso-download.json", JSON.stringify(data['3166-1'], null, 2))
-}
+  // Overwrite the existing data/download.json file with the latest data.
+  await fs.writeFile("../data/tz-download.json", JSON.stringify(data, null, 2));
+};
 
 /**
  * Sorts a map by the name of the values.
@@ -40,48 +28,91 @@ const downloadISO = async () => {
  * @returns {Record<string, string>}
  */
 const sortMap = (map) => {
-	const sorted = Object.fromEntries(Object.entries(map).sort((a, b) => {
-		if (a[1].name < b[1].name) return -1
-		if (a[1].name > b[1].name) return 1
-		return 0
-	}))
+  const sorted = Object.fromEntries(
+    Object.entries(map).sort((a, b) => {
+      if (a[1].name < b[1].name) return -1;
+      if (a[1].name > b[1].name) return 1;
+      return 0;
+    })
+  );
 
-	return sorted
-}
+  return sorted;
+};
+
+/**
+ * Adds missing timezones to the list of supported timezones.
+ * @param {TimezoneMap} tzMap
+ * @returns {Promise<void>}
+ */
+const checkMissing = async (tzMap) => {
+  // Get the list of supported timezones and find any timezones that do not exist in moment-timezone db.
+  // This is typically because the timezone has been deprecated or browser implementations differ.
+  // This TC39 proposal describes the problem: https://github.com/tc39/proposal-canonical-tz
+  const supported = Intl.supportedValuesOf("timeZone");
+  const tzMapKeys = Object.keys(tzMap);
+  const missing = supported.filter((zone) => !tzMapKeys.includes(zone));
+
+  // If there are missing timezones, print them to the console and fail
+  // the build.
+  if (missing.length > 0) {
+    console.log("Missing timezones:", missing);
+    throw new Error("Missing timezones");
+  }
+};
 
 /**
  * Parses the moment timezone data and returns an array of countries.
  * @returns {Promise<TimezoneMap>}
  */
 const parseTimezones = async () => {
-	const { countries, zones } = JSON.parse(await fs.readFile("../data/tz-download.json"))
-	const iso = JSON.parse(await fs.readFile("../data/iso-download.json"))
+  const { countries, zones } = JSON.parse(
+    await fs.readFile("../data/tz-download.json")
+  );
 
-	/** @type {TimezoneMap} */
-	const timezones = {}
+  /** @type {TimezoneMap} */
+  const timezones = {};
 
-	/** @type {CountryCodeMap} */
-	const countryCodes = {}
+  for (const zone of Object.keys(zones)) {
+    const country = countries[zones[zone].countries[0]];
+    timezones[zone] = country.abbr;
+  }
 
-	for (const zone of Object.keys(zones)) {
-		const country = countries[ zones[ zone ].countries[ 0 ] ]
-		const abbr = country.abbr
-		const iso_country = iso.find(country => country.alpha_2 === abbr)
-		const name = iso_country.common_name ?? iso_country.name
+  // Sort the timezones by name.
+  const timezonesSorted = sortMap(timezones);
 
-		timezones[ zone ] = abbr
-		countryCodes[ abbr ] = name
-	}
-	
-	// Sort the timezones by name.
-	const timezonesSorted = sortMap(timezones)
-	const countryCodesSorted = sortMap(countryCodes)
+  // Add missing deprecated timezones.
+  const missingTimezones = JSON.parse(
+    await fs.readFile("../data/missing.json")
+  );
+  for (const [zone, country] of Object.entries(missingTimezones)) {
+    timezonesSorted[zone] = country;
+  }
 
-	// Write the parsed data to files.
-	await fs.writeFile("../data/tzcode.json", JSON.stringify(timezonesSorted, null, 2))
-	await fs.writeFile("../data/codecountry.json", JSON.stringify(countryCodesSorted, null, 2))
-}
+  // Check for missing timezones.
+  await checkMissing(timezonesSorted);
 
-await downloadMoment()
-await downloadISO()
-await parseTimezones()
+  // Create a map of country codes.
+  /** @type {CountryCodeMap} */
+  const countryCodes = {};
+  const nameFormatter = new Intl.DisplayNames("en", { type: "region" });
+
+  for (const code of Object.values(timezonesSorted)) {
+    const name = nameFormatter.of(code);
+    countryCodes[code] = name;
+  }
+
+  const countryCodesSorted = sortMap(countryCodes);
+
+  // Write the parsed data to files.
+  await fs.writeFile(
+    "../data/tzcode.json",
+    JSON.stringify(timezonesSorted, null, 2)
+  );
+  await fs.writeFile(
+    "../data/codecountry.json",
+    JSON.stringify(countryCodesSorted, null, 2)
+  );
+};
+
+await downloadMoment();
+await parseTimezones();
